83daf8df71a0b83d48eb347d2e898d19714c3d01
Vicrrs <enricofermi20192@gmail.com>
2024-02-24 20:51:51 -0300

    grafico inicial

---
+++ visualização/graficos.py
@@ -0,0 +1,13 @@
+import plotly.express as px
+
+def cria_mapa_receita(receita_estados):
+    fig_mapa_receita = px.scatter_geo(receita_estados,
+                                      lat='lat',
+                                      lon='lon',
+                                      scope='south america',
+                                      size='Preço',
+                                      template='seaborn',
+                                      hover_name='Local da compra',
+                                      hover_data={'lat': False, 'lon': False},
+                                      title='Receita do estado')
+    return fig_mapa_receita
