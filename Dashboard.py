58b2794e2a9ee92844098abb2931ae4b69bcfd6c
Vicrrs <enricofermi20192@gmail.com>
2024-02-24 23:05:01 -0300

    Dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -12,7 +12,7 @@ with httpx.Client() as client:
     response = client.get(url)
     html_content = response.json()
     dados = pd.DataFrame.from_dict(html_content)
-    dados['Data da compra'] = pd.to_datetime(dados['Data da compra'], format='%d/%m%Y')
+    dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
     print(dados)
     
     
@@ -29,8 +29,8 @@ receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
 receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
-receita_mensal['Ano'] = receita_mensal['Data  da Compra'].dt.year
-receita_mensal['Mes'] = receita_mensal['Data  da Compra'].dt.month_name()
+receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
 
 # Gráficos
 fig_mapa_receita = px.scatter_geo(receita_estados,
