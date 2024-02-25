31eed871e8a780432215f4ed3579b1b843570a57
Vicrrs <enricofermi20192@gmail.com>
2024-02-24 22:59:48 -0300

    tabela receita mensal

--- visualizacao/tabelas.py
+++ visualizacao/tabelas.py
@@ -1,4 +1,14 @@
+import pandas as pd
+
 def cria_tabela_receita_estados(dados):
     receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
     receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
     return receita_estados
+
+
+def cria_tabela_receita_mensal(dados):
+    receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
+    receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
+    receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
+
+    return receita_mensal
