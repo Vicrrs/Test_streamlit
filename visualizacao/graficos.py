9ad20b9869eeb60bc376e7f573a04fe557cdf576
Vicrrs <enricofermi20192@gmail.com>
2024-02-24 22:59:35 -0300

    receita mensal

--- visualizacao/graficos.py
+++ visualizacao/graficos.py
@@ -11,3 +11,16 @@ def cria_mapa_receita(receita_estados):
                                       hover_data={'lat': False, 'lon': False},
                                       title='Receita do estado')
     return fig_mapa_receita
+
+def cria_grafico_receita_mensal(receita_mensal):
+    fig_receita_mensal = px.line(receita_mensal,
+                                 x='Mes',
+                                 y='Preço',
+                                 markers=True, 
+                                 range_y=[0, receita_mensal['Preço'].max()], 
+                                 color='Ano',
+                                 line_dash='Ano',
+                                 title='Receita Mensal')
+    
+    fig_receita_mensal.update_layout(yaxis_title='Receita')
+    return fig_receita_mensal
