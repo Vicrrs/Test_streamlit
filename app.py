299d9239844bfe65fffb9c2b30bb6d6e8d87a92b
Vicrrs <enricofermi20192@gmail.com>
2024-02-25 00:20:16 -0300

    app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from dados.carrega_dados import carregar_dados
-from visualizacao.graficos import cria_mapa_receita, cria_grafico_receita_mensal
-from visualizacao.tabelas import cria_tabela_receita_estados, cria_tabela_receita_mensal
+from visualizacao.graficos import cria_mapa_receita, cria_grafico_receita_mensal, cria_grafico_receita_estados, cria_grafico_receita_categorias
+from visualizacao.tabelas import cria_tabela_receita_estados, cria_tabela_receita_mensal, cria_tabela_receita_categoria
 from utilidades.formatadores import formata_numero
 
 st.set_page_config(layout="wide")
@@ -15,12 +15,20 @@ fig_mapa_receita = cria_mapa_receita(receita_estados)
 receita_mensal = cria_tabela_receita_mensal(dados)
 fig_receita_mensal = cria_grafico_receita_mensal(receita_mensal)
 
+fig_receita_estados = cria_grafico_receita_estados(receita_estados)
+
+receita_categoria = cria_tabela_receita_categoria(dados)
+fig_receita_categorias = cria_grafico_receita_categorias(receita_categoria)
+
 coluna1, coluna2 = st.columns(2)
 with coluna1:
     st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
     st.plotly_chart(fig_mapa_receita, use_container_width=True)
+    st.plotly_chart(fig_receita_estados, use_container_width=True)
+
 with coluna2:
     st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
     st.plotly_chart(fig_receita_mensal)
+    st.plotly_chart(fig_receita_categorias, use_container_width=True)
 
 st.dataframe(dados)
