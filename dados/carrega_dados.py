8939728ab4a7d4ac01ef04c4f3ed1033f6bfa49b
Vicrrs <enricofermi20192@gmail.com>
2024-02-24 22:59:24 -0300

    dados data compra

--- dados/carrega_dados.py
+++ dados/carrega_dados.py
@@ -5,5 +5,6 @@ def carregar_dados():
     url = 'https://labdados.com/produtos'
     with httpx.Client() as client:
         response = client.get(url)
-        dados = pd.Dataframe.from_dict(response.json())
+        dados = pd.DataFrame.from_dict(response.json())
+        dados['Data da Compra'] = pd.to_datetime(dados['Data da Compra'], format='%d/%m/%Y')
         return dados
