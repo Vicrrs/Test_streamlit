91d658de2692745533fd5704878b43b29ac88789
Vicrrs <enricofermi20192@gmail.com>
2024-02-25 01:36:52 -0300

    adicionando abas

--- Dashboard.py
+++ Dashboard.py
@@ -72,15 +72,30 @@ fig_receita_categorias.update_layout(yaxis_title='Receita')
 
 
 # Visualização no Streamlit
-coluna1, coluna2 = st.columns(2)
-with coluna1:
-    st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
-    st.plotly_chart(fig_mapa_receita, use_container_width=True)
-    st.plotly_chart(fig_receita_estados, use_container_width=True)
-with coluna2:
-    st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
-    st.plotly_chart(fig_receita_mensal)
-    st.plotly_chart(fig_receita_categorias, use_container_width=True)
 
-    
-st.dataframe(dados)
+aba1, aba2, aba3 = st.tabs(['Receita', 'Quantidade de vendas', 'Vendendores'])
+with aba1:
+    coluna1, coluna2 = st.columns(2)
+    with coluna1:
+        st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+        st.plotly_chart(fig_mapa_receita, use_container_width=True)
+        st.plotly_chart(fig_receita_estados, use_container_width=True)
+    with coluna2:
+        st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
+        st.plotly_chart(fig_receita_mensal)
+        st.plotly_chart(fig_receita_categorias, use_container_width=True)
+        
+with aba2:
+    coluna1, coluna2 = st.columns(2)
+    with coluna1:
+        st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+    with coluna2:
+        st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
+        
+with aba3:
+    coluna1, coluna2 = st.columns(2)
+    with coluna1:
+        st.metric('Receita', formata_numero(dados['Preço'].sum(), 'R$'))
+    with coluna2:
+        st.metric('Quantidade de Vendas', formata_numero(dados.shape[0]))
+
